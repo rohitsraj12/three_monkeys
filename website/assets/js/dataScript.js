03a194defcdbc32c9071aa5ba8058e8ba8d201ac
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-18 18:05:40 +0530

    Create dataScript.js

---
+++ website/assets/js/dataScript.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".header__contact .address").text('#12, 4th floor, signature mall, Bangalore');
+    $(".header__contact .tel").text('+91 999 999 9999');
+    $(".header__contact .mailto").text('contact@mail.com');
+    $(".header__contact .timing").text('10:00am to 11.30pm');
+})
