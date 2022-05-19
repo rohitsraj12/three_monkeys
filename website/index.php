2b5ac3319433a6aba3a72c81dade1e36a694db5e
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-19 17:35:21 +0530

    Update index.php

--- website/index.php
+++ website/index.php
@@ -114,12 +114,113 @@
             <main>
 
                 <section class="section__about">
-                    
+                    <div class="wrap__container py-5">
+                        <div class="row py-5 align-items-center g-4">
+
+                            <div class="col-md-6">
+                                <article>
+                                    <header class="article__header pb-4">
+                                        <p class="primary__header-sub sub__style">
+                                            About Us
+                                        </p>
+                                        <p class="primary__header">
+                                            Three Monkeys
+                                        </p>
+                                    </header>
+
+                                    <div class="article__body">
+                                        <p>
+                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe neque veritatis magni illo inventore sed ducimus, dicta fuga totam et doloribus fugit dignissimos alias deleniti incidunt perspiciatis eius? Laborum, hic.
+                                        </p>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam dolore quasi at voluptatem blanditiis tenetur quibusdam laudantium, illo autem sit?
+                                        </p>
+                                        <ul>
+                                            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </li>
+                                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur hic natus iusto! </li>
+                                            <li>Lorem ipsum, dolor sit amet consectetur adipisicing. </li>
+                                        </ul>
+                                    </div>
+
+                                </article>
+                            </div>
+
+                            <div class="col-md-1"></div>
+
+                            <div class="col-md-5">
+                                <figure>
+                                    <img class="w-100" src="assets/img/bg/about-bg.jpg" alt="">
+                                </figure>
+                            </div>
+
+                        </div>
+                    </div>
                 </section>
 
+                <section class="section__why py-5">
+                    <div class="wrap__container py-5">
+                        <header class="section__header">
+                            <p class="primary__header-sub sub__style">
+                                why us
+                            </p>
+                            <p class="primary__header">
+                                Why Choose Our Restaurant
+                            </p>
+                        </header>
 
-                <section class="section__why">
-                    
+                        <div class="section__body">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <section>
+                                        <article>
+                                            <header class="article">
+                                                <h2>
+                                                    Lorem, ipsum dolor.
+                                                </h2>
+                                            </header>
+                                            <article class="article__body">
+                                                <p>
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque temporibus nulla molestias inventore accusantium doloremque necessitatibus fugit reprehenderit delectus eos?
+                                                </p>
+                                            </article>
+                                        </article>
+                                    </section>
+                                </div>
+                                <div class="col-md-4">
+                                    <section>
+                                        <article>
+                                            <header class="article">
+                                                <h2>
+                                                    Lorem, ipsum dolor.
+                                                </h2>
+                                            </header>
+                                            <article class="article__body">
+                                                <p>
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque temporibus nulla molestias inventore accusantium doloremque necessitatibus fugit reprehenderit delectus eos?
+                                                </p>
+                                            </article>
+                                        </article>
+                                    </section>
+                                </div>
+                                <div class="col-md-4">
+                                    <section>
+                                        <article>
+                                            <header class="article">
+                                                <h2>
+                                                    Lorem, ipsum dolor.
+                                                </h2>
+                                            </header>
+                                            <article class="article__body">
+                                                <p>
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque temporibus nulla molestias inventore accusantium doloremque necessitatibus fugit reprehenderit delectus eos?
+                                                </p>
+                                            </article>
+                                        </article>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
