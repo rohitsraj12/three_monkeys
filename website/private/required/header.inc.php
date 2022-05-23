57cf867a605be6049721b10909b248deaeaa9391
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-23 17:29:11 +0530

    Update header.inc.php

---
+++ website/private/required/header.inc.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/center_nav.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css">
+    
+    <!-- script -->
+</head>
+<body>
+    <div class="body__wrap">
+
+        <div class="body__header body__header-absolute">
+            <div class="body__header-top">
+                <div class="header__container">
+                    <div class="row">
+                        <div class="col-8">
+                            <ul class="header__contact">
+                                <li class="header__contact-list"><i class="fa fa-location-arrow" aria-hidden="true"></i><a class="header__contact-link address" href="">Loading..</a></li>
+                                <li class="header__contact-list"><i class="fa fa-phone" aria-hidden="true"></i><a class="header__contact-link tel" href="">Loading..</a></li>
+                                <!-- <li class="header__contact-list"><a class="header__contact-link mailto" href="">Loading..</a></li> -->
+                                <li class="header__contact-list"><i class="fa fa-clock-o" aria-hidden="true"></i><span class="header__contact-link timing" href="">Loading..</span></li>
+                            </ul>
+                        </div>
+                        <!-- end header contact -->
+                        <div class="col-4">
+                            <ul class="header__social">
+                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                            </ul>
+                            <!-- end header social -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end body header top -->
+            
+            <div class="body__header-main">
+                <!-- <header class="header__container">
+                    <div class="header__brand py-1">
+                        <a href="">
+                            <img src="<?php echo base_url();?>assets/img/brand/logo.svg" alt="LOGO">
+                        </a>
+                    </div>
+
+                    <nav class="header__nav">
+                        <ul class="main__nav">
+                            <li class="nav__list"><a href="<?php echo base_url();?>" class="nav__link">Home</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>about/" class="nav__link">About</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Menu</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Gallery</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Crafted Beer</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Contact Us</a></li>
+                        </ul>
+                    </nav>
+                </header> -->
+
+
+                
+                <header class="header__container-center">
+                   
+
+                    <nav class="header__nav">
+                        <ul class="main__nav">
+                            <li class="nav__list"><a href="<?php echo base_url();?>" class="nav__link">Home</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>about/" class="nav__link">About</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Crafted Beer</a></li>
+                        </ul>
+                        <div class="header__brand">
+                            
+                            <div class="wrap__brand">
+                                <a href="">
+                                    <img src="<?php echo base_url();?>assets/img/brand/logo.svg" alt="LOGO">
+                                </a>
+                            </div>
+                        </div>
+                        <ul class="main__nav">
+                            <li class="nav__list"><a href="" class="nav__link">Menu</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Gallery</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Contact Us</a></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+            <!-- end body header main -->
+
+        </div>
+        <!-- end body header -->
