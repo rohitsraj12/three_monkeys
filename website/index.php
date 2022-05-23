d264272822d924cd91c18e0581ef02e02844fb86
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-23 17:29:04 +0530

    Update index.php

--- website/index.php
+++ website/index.php
@@ -1,85 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+<?php
 
-    <!-- stylesheet -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    
-    <!-- script -->
-</head>
-<body>
-    <div class="body__wrap">
+    include("private/config/config.php");
+    include("private/required/header.inc.php");
 
-        <div class="body__header body__header-absolute">
-            <div class="body__header-top">
-                <div class="header__container">
-                    <div class="row">
-                        <div class="col-8">
-                            <ul class="header__contact">
-                                <li class="header__contact-list"><i class="fa fa-location-arrow" aria-hidden="true"></i><a class="header__contact-link address" href="">Loading..</a></li>
-                                <li class="header__contact-list"><i class="fa fa-phone" aria-hidden="true"></i><a class="header__contact-link tel" href="">Loading..</a></li>
-                                <!-- <li class="header__contact-list"><a class="header__contact-link mailto" href="">Loading..</a></li> -->
-                                <li class="header__contact-list"><i class="fa fa-clock-o" aria-hidden="true"></i><span class="header__contact-link timing" href="">Loading..</span></li>
-                            </ul>
-                        </div>
-                        <!-- end header contact -->
-                        <div class="col-4">
-                            <ul class="header__social">
-                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
-                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-                            </ul>
-                            <!-- end header social -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- end body header top -->
-            
-            <div class="body__header-main py-3">
-                <header class="header__container">
-                    <div class="header__brand">
-                        <a href="">
-                            <img src="assets/img/brand/logo.png" alt="LOGO">
-                        </a>
-                    </div>
-                    <!-- end header brand -->
-
-                    <nav class="header__nav">
-                        <ul class="main__nav">
-                            <li class="nav__list"><a href="" class="nav__link">Home</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">About</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Menu</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Gallery</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Blog</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Contact Us</a></li>
-                        </ul>
-                    </nav>
-                </header>
-            </div>
-            <!-- end body header main -->
-
-        </div>
-        <!-- end body header -->
-
-        <div class="body__banner">
+?>
+        <div class="body__banner home__banner">
             <div class="banner__image">
 
                 <div class="banner__image-desktop">
-                    <img src="assets/img/banner/home/banner-1-lg.jpg" alt="">
+                    <img src="<?php echo base_url();?>assets/img/banner/home/banner-1-lg.jpg" alt="">
                 </div>
                 <!-- end banner image desktop -->
                 
                 <div class="banner__image-mobile">
-                    <img src="assets/img/banner/home/banner-1-sm.jpg" alt="">
+                    <img src="<?php echo base_url();?>assets/img/banner/home/banner-1-sm.jpg" alt="">
                 </div>
                 <!-- end banner image mobile -->
 
@@ -90,19 +24,20 @@
                 <div class="banner__content-wrap">
                     <section>
                         <article>
-                            <header class="banner__header">
+                            <!-- <header class="banner__header">
                                 <h2>
                                     Welcome to 
                                 </h2>
-                                <h2>
-                                    three monkeys
-                                </h2>
-                            </header>
-                            <div class="banner__body">
+                                <h2></h2>
+                            </header> -->
+                            <figure>
+                                <img src="assets/img/brand/logo_banner.svg" alt="">
+                            </figure>
+                            <!-- <div class="banner__body">
                                 <p>
                                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic pariatur quibusdam similique quisquam minima eveniet libero, sit dolore id perferendis?
                                 </p>
-                            </div>
+                            </div> -->
                         </article>
                     </section>
                 </div>
@@ -149,7 +84,7 @@
 
                             <div class="col-md-5">
                                 <figure>
-                                    <img class="w-100" src="assets/img/bg/about-bg.jpg" alt="">
+                                    <img class="w-100" src="<?php echo base_url();?>assets/img/bg/about-bg.jpg" alt="">
                                 </figure>
                             </div>
 
@@ -225,17 +160,9 @@
             </main>
         </div>
         <!-- end body container -->
+        
+<?php
 
-        <div class="body__footer"></div>
-        <!-- end body footer -->
+    include("private/required/footer.inc.php");
 
-    </div>
-    <!-- end body wrap -->
-
-    <!-- script -->
-    <script src="assets/js/jQuery.v3.6.0.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/dataScript.js"></script>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
+?>
