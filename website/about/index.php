29e1a7d9758acf2416dae68b201486094a7645f8
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-23 17:28:28 +0530

    Create index.php

---
+++ website/about/index.php
@@ -0,0 +1,222 @@
+<?php
+
+    include("../private/config/config.php");
+    include("../private/required/header.inc.php");
+
+?>
+        <div class="body__banner other__banner">
+            <div class="banner__image">
+
+                <div class="banner__image-desktop">
+                    <img src="<?php echo base_url();?>assets/img/banner/about_us.jpg" alt="">
+                </div>
+                <!-- end banner image desktop -->
+                
+                <div class="banner__image-mobile">
+                    <img src="<?php echo base_url();?>assets/img/banner/home/banner-1-sm.jpg" alt="">
+                </div>
+                <!-- end banner image mobile -->
+
+            </div>
+            <!-- end banner image -->
+
+            <div class="banner__content">
+                <div class="banner__content-wrap">
+                    <section class="wrap__container">
+                        <article>
+                            <header class="banner__header">
+                                <h2>
+                                    About Us
+                                </h2>
+                            </header>
+                            
+                            <div class="banner__body">
+                                <ul>
+                                    <li><a href="">Home</a></li>
+                                    <li><a href="">About Us</a></li>
+                                </ul>
+
+                            </div>
+                        </article>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <!-- end body banner -->
+
+        <div class="body__container">
+            <main>
+
+                <section class="section__about">
+                    <div class="container py-5">
+                        <div class="row py-5 align-items-center g-4">
+
+                            <div class="col-md-12">
+                                <article>
+                                    <header class="article__header pb-4 text-center">
+                                        <p class="primary__header-sub sub__style-center">
+                                            Our story
+                                        </p>
+                                        <p class="primary__header">
+                                            The Monkey Kingdom- Hampi
+                                        </p>
+                                    </header>
+
+                                    <div class="article__body text-center">
+                                        <div class="pb-5">
+                                            <div class="row justify-content-center">
+                                                <div class="col-md-6">
+                                                    <figure>
+                                                        <img class="w-100" src="<?php echo base_url();?>assets/img/about/001.jpg" alt="">
+                                                    </figure>
+                                                </div>
+                                                <div class="col-12">
+                                                    
+                                                    <!-- <h6 class="article__header">
+                                                        Kishkinda- The Mythical Monkey Kingdom in Hampi
+                                                    </h6> -->
+
+                                                    <p>
+                                                        Isn’t it unusual that a modern pub has a traditional look?
+                                                    </p>
+                                                    <p>
+                                                        Well, the Gopalan Group is always known to blend tradition with modern and this time also it is no different.
+                                                    </p>
+                                                    <p>
+                                                        Our brand new venture Three Monkeys- Micro Brewery, Grand Mercure, Gopalan Mall, is designed keeping the Hoysala Architecture in mind.
+                                                    </p>
+                                                    <p>
+                                                        So, let’s take you down the mythological lane!!!
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                       <div class="pb-4">
+                                            <div class="row justify-content-center">
+                                                
+                                                <div class="col-md-6">
+                                                    <figure>
+                                                        <img class="w-100" src="<?php echo base_url();?>assets/img/about/001.jpg" alt="">
+                                                    </figure>
+                                                </div>
+                                                <div class="col-12">
+                                                    <h6 class="article__header">
+                                                        Kishkinda- The Mythical Monkey Kingdom in Hampi
+                                                    </h6>
+                                                    <p>
+                                                        Kishkinda, a very popular mythical story associates the landscape in Hampi with the Hindu epic Ramayana and some parts of Mahabharata as well. According to the mystical history, Kishkinda used to be the kingdom of the Vanara King, Sugreeva.
+                                                    </p>
+                                                    <p>
+                                                        The Anjaneya Hill, located across the river Tungabhadra is the birth place of Lord Hanuman, the Monkey King.
+                                                    </p>
+                                                    <p>
+                                                        It is said that when Ravana abducted Sita, lord Rama along with Lakshmana reached Hampi in search of her. Hanuman, the general of the monkey king Sugreeva mistook them to be spies from the rebel of Sugreeva. On hearing their story Hanuman brings them to Sugreeva. He eventually takes Rama and Lakshmana to a cave and shows them a set of jewels. Rama recognizes them as that of his wife Sita. Sugreeva then explains them that Sita dropped them at the site called Kishkinda when Ravana abducted her on his flying chariot.
+                                                    </p>
+                                                    <p>
+                                                        On hearing this, Hanuman decides to fly to Lanka and comes back with the news that Sita indeed is in the custody of Ravana. Hanuman offers Rama with the help of his monkey army to make a bridge across and attack Lanka which eventually becomes a success.
+                                                    </p>
+                                                    <p>
+                                                        Hampi probably has many more icons of Hanuman than any other Gods.
+                                                    </p>
+                                                    <p>
+                                                        The temples in Hampi are generously curved with mythical themes. Because it was a Monkey’s Kingdom the images of monkeys are splendidly carved on the walls and pillars of the temples of Hampi.
+                                                    </p>
+                                                    <p>
+                                                        Interesting, isn’t it?
+                                                    </p>
+                                                </div>
+                                            </div>
+                                       </div>
+                                       <div class="pb-4 text-center">
+                                           <div class="row justify-content-center">
+                                                <div class="col-md-6">
+                                                    <figure>
+                                                        <img class="w-100" src="<?php echo base_url();?>assets/img/about/001.jpg" alt="">
+                                                    </figure>
+                                               </div>
+                                               <div class="col-12">
+                                                   
+                                                    <p>
+                                                        So, while you enjoy your drinks at Three Monkeys we make sure to give you a feel of our culture and history.
+                                                    </p>
+                                                    <p>
+                                                        Cheers to some awesome cocktails and some amazing mythological tales.
+                                                    </p>
+                                               </div>
+                                           </div>
+                                       </div>
+                                    </div>
+
+                                </article>
+                            </div>
+
+                            <!-- <div class="col-md-1"></div>
+
+                            <div class="col-md-5">
+                                <figure>
+                                    <img class="w-100" src="<?php echo base_url();?>assets/img/bg/about-bg.jpg" alt="">
+                                </figure>
+                            </div> -->
+
+                        </div>
+                    </div>
+                </section>
+
+                <section class="section__why py-5">
+                    <div class="wrap__container py-5">
+                        <header class="section__header text-center">
+                            <p class="primary__header-sub sub__style-center">
+                                Story of
+                            </p>
+                            <p class="primary__header">
+                                Three Monkeys Logo 
+                            </p>
+                        </header>
+
+                        <div class="section__body">
+                            <div class="row justify-content-center">
+                                
+                                <div class="col-md-4 text-center">
+                                    <img class="w-50" src="<?php echo base_url();?>assets/img/brand/logo.svg" alt="">
+                                </div>
+                                <div class="col-md-8">
+                                    <section>
+                                        <article>
+                                            <header class="article">
+                                                <h2 class="article__header">
+                                                    Three Monkeys- Micro Brewery
+                                                </h2>
+                                            </header>
+                                            <article class="article__body">
+                                                <p>
+                                                    What’s behind the logo!
+                                                </p>
+                                                <p>
+                                                    The evolution of Wise Monkeys to Smart Monkeys. The father of the nation, Mahatma Gandhi taught us the concept of the three Wise Monkeys. See no Evil! Hear no Evil! Speak no Evil!
+                                                </p>
+                                                <p>
+                                                    But in today’s day, being wise is not enough, you need to be smart. Street Smart!!!  See and then Decide. Hear and then share your Opinion. Speak up for the Right.
+                                                </p>
+                                                <p>
+                                                    Our logo encourages the same evolution.
+                                                </p>
+                                                <p>
+                                                    Be wise, but in a smart way!!!
+                                                </p>
+                                            </article>
+                                        </article>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
+        <!-- end body container -->
+        
+<?php
+
+    include("../private/required/footer.inc.php");
+
+?>
