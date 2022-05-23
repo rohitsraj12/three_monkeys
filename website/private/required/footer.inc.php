3cae3eb8125d716a205e67ede55388f38ed93282
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-23 17:29:09 +0530

    Update footer.inc.php

---
+++ website/private/required/footer.inc.php
@@ -0,0 +1,14 @@
+
+        <div class="body__footer"></div>
+        <!-- end body footer -->
+
+    </div>
+    <!-- end body wrap -->
+
+    <!-- script -->
+    <script src="<?php echo base_url();?>assets/js/jQuery.v3.6.0.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/dataScript.js"></script>
+    <script src="<?php echo base_url();?>assets/js/script.js"></script>
+</body>
+</html>
