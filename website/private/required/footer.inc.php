6072a6bff642bcedaf10b519591e54aa69e8e821
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-26 17:05:05 +0530

    Update footer.inc.php

--- website/private/required/footer.inc.php
+++ website/private/required/footer.inc.php
@@ -1,14 +1,135 @@
 
-        <div class="body__footer"></div>
+        <div class="body__footer pt-5">
+            <footer>
+                <div class="footer__top mb-5">
+                    <div class="wrap__container">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-4">
+                                <article>
+                                    <figure class="footer__figur">
+                                        <a href="">
+                                            <img width="150px" src="<?php echo base_url();?>assets/img/brand/logo.svg" alt="LOGO">
+                                        </a>
+                                    </figure>
+                                    <p>
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id maiores sed nostrum quaerat, recusandae eveniet! Temporibus amet, libero quo saepe facilis aliquam, blanditiis 
+                                    </p>
+                                    <footer>
+                                        <ul class="footer__social">
+                                            <!-- <li class="footer__social-list"><a href="" class="footer__social-link"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li> -->
+                                            <li class="footer__social-list"><a href="" class="footer__social-link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                            <li class="footer__social-list"><a href="" class="footer__social-link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                            <li class="footer__social-list"><a href="" class="footer__social-link"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                                        </ul>
+                                    </footer>
+                                </article>
+                            </div>
+                            <div class="col-sm-6 col-md-1">
+
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <article class="footer__nav">
+                                    <header class="footer__header">
+                                        Quick Links
+                                    </header>
+
+                                    <ul>
+                                        <li class="footer__nav-list"><a href="<a href="<?php echo base_url();?>" class="footer__nav-link">Home</a></li>
+                                        <li class="footer__nav-list"><a href="<?php echo base_url();?>about/" class="footer__nav-link">About</a></li>
+                                        <li class="footer__nav-list"><a href="<?php echo base_url();?>craft_beers/" class="footer__nav-link">Crafted Beer</a></li>
+                                        <li class="footer__nav-list"><a href="<?php echo base_url();?>menu/" class="footer__nav-link">Menu</a></li>
+                                        <li class="footer__nav-list"><a href="<?php echo base_url();?>gallery/" class="footer__nav-link">Gallery</a></li>
+                                        <li class="footer__nav-list"><a href="<?php echo base_url();?>contact_us/" class="footer__nav-link">Contact Us</a></li>
+                                        <li class="footer__nav-list"><a href="<?php echo base_url();?>contact_us/reservation.php" class="footer__nav-link">Reservation</a></li>
+                                    </ul>
+                                </article>
+                            </div>
+                            
+                            <div class="col-sm-6 col-md-1">
+                                
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <div class="footer__contact">
+                                    
+                                    <header class="footer__header">
+                                        Contact Details
+                                    </header>
+
+                                    <ul>
+                                        <li class="footer__contact-list">
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                            <a class="footer__contact-link tel" href="">Loading..</a>
+                                        </li>
+                                        <li class="footer__contact-list">
+                                            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                            <a class="footer__contact-link mailto" href="">Loading..</a>
+                                        </li>
+                                        <li class="footer__contact-list">
+                                            <i class="fa fa-location-arrow" aria-hidden="true"></i>
+                                            <a class="footer__contact-link address" >Loading..</a>
+                                        </li>
+                                    </ul>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="footer__copy-right py-3">
+                    <div class="wrap__container text-center">
+                        <p class="m-0 p-0">
+                            copyright &copy; <script>document.write(new Date().getFullYear());</script> | <a class="text-light" href="#" target="_blank">terms and condition</a> | <a class="text-light" href="<?php echo base_url();?>" target="_blank">threemonkeys.com</a>
+                        </p>
+                    </div>
+                </div>
+            </footer>
+        </div>
         <!-- end body footer -->
 
     </div>
     <!-- end body wrap -->
 
+    <div class="section__fixed">
+        <div class="reservation__button">
+            <a href="<?php echo base_url();?>contact_us/reservation.php"><i class="fa fa-cutlery" aria-hidden="true"></i>Reservation</a>
+        </div>
+    </div>
+
     <!-- script -->
     <script src="<?php echo base_url();?>assets/js/jQuery.v3.6.0.min.js"></script>
     <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url();?>assets/js/dataScript.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+
     <script src="<?php echo base_url();?>assets/js/script.js"></script>
+
+    <script>
+      AOS.init();
+    </script>
+
+    <?php 
+        if($nav_active == "gallery"){
+            ?>
+                <script>
+                    $(document).ready(function(){
+                        $(".fancybox").attr("data-fancybox","mygallery");
+                        // assign captions and title from alt-attributes of images:
+                        $(".fancybox").each(function(){
+                            $(this).attr("data-caption", $(this).find("img").attr("alt"));
+                            $(this).attr("title", $(this).find("img").attr("alt"));
+                        });
+                        // start fancybox:
+                        $(".fancybox").fancybox();
+                    })
+                </script>
+
+            <?php
+        }
+    
+    
+    ?>
+    
 </body>
 </html>
