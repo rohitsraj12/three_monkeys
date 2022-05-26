7e5008811c030d1ab76b3113c987262133d46780
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-26 17:04:46 +0530

    Update dataScript.js

--- website/assets/js/dataScript.js
+++ website/assets/js/dataScript.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-    $(".header__contact .address").text('#12, 4th floor, signature mall, Bangalore');
+    $(".header__contact .address").text('Grand Mercure, Gopalan Mall');
     $(".header__contact .tel").text('+91 999 999 9999');
     $(".header__contact .mailto").text('contact@mail.com');
     $(".header__contact .timing").text('10:00am to 11.30pm');
+
+    //footer
+    $(".footer__contact .address").text('Grand Mercure, Gopalan Mall, Bangalore');
+    $(".footer__contact .tel").text('+91 999 999 9999');
+    $(".footer__contact .mailto").text('contact@mail.com');
+    $(".footer__contact .timing").text('10:00am to 11.30pm');
+
+    //contact us page
+    $(".section__contact_us .address").text('Grand Mercure, Gopalan Mall, Bangalore');
+    $(".section__contact_us .tel").text('+91 999 999 9999');
+    $(".section__contact_us .mailto").text('contact@mail.com');
+    $(".section__contact_us .timing").text('10:00am to 11.30pm');
+    
+    //contact us page
+    $(".reservation__contact .address").text('Grand Mercure, Gopalan Mall, Bangalore');
+    $(".reservation__contact .tel").text('+91 999 999 9999');
+    $(".reservation__contact .mailto").text('contact@mail.com');
+    $(".reservation__contact .timing").text('10:00am to 11.30pm');
 })
