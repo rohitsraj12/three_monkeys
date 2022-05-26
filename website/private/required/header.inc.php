ea516bf93e9bef197c0ac80f0dc573f863cfe506
rohitsraj12 <rohitsraj12@gmail.com>
2022-05-26 17:05:07 +0530

    Update header.inc.php

--- website/private/required/header.inc.php
+++ website/private/required/header.inc.php
@@ -4,23 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title><?php echo $page_title; ?></title>
 
     <!-- stylesheet -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/center_nav.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/gallery.css">
     
     <!-- script -->
 </head>
 <body>
     <div class="body__wrap">
 
-        <div class="body__header body__header-absolute">
+        <div class="body__header body__header-absolute"  data-aos="fade-in"
+                    data-aos-offset="100"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in">
             <div class="body__header-top">
-                <div class="header__container">
+                <div class="header__container-center">
                     <div class="row">
                         <div class="col-8">
                             <ul class="header__contact">
@@ -33,7 +42,7 @@
                         <!-- end header contact -->
                         <div class="col-4">
                             <ul class="header__social">
-                                <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+                                <!-- <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li> -->
                                 <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                                 <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                                 <li class="header__social-list"><a href="" class="header__social-link"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
@@ -59,7 +68,7 @@
                             <li class="nav__list"><a href="<?php echo base_url();?>about/" class="nav__link">About</a></li>
                             <li class="nav__list"><a href="" class="nav__link">Menu</a></li>
                             <li class="nav__list"><a href="" class="nav__link">Gallery</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Crafted Beer</a></li>
+                            <li class="nav__list"><a href="" class="nav__link">Craft Beer</a></li>
                             <li class="nav__list"><a href="" class="nav__link">Contact Us</a></li>
                         </ul>
                     </nav>
@@ -72,9 +81,9 @@
 
                     <nav class="header__nav">
                         <ul class="main__nav">
-                            <li class="nav__list"><a href="<?php echo base_url();?>" class="nav__link">Home</a></li>
-                            <li class="nav__list"><a href="<?php echo base_url();?>about/" class="nav__link">About</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Crafted Beer</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>" class="nav__link <?php if($nav_active == "home"){ echo "active__nav" ; }?>">Home</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>about/" class="nav__link <?php if($nav_active == "about"){ echo "active__nav" ; }?>">About</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>craft_beers/" class="nav__link <?php if($nav_active == "craft"){ echo "active__nav" ; }?>">Craft Beer</a></li>
                         </ul>
                         <div class="header__brand">
                             
@@ -85,9 +94,10 @@
                             </div>
                         </div>
                         <ul class="main__nav">
-                            <li class="nav__list"><a href="" class="nav__link">Menu</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Gallery</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">Contact Us</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>menu/" class="nav__link <?php if($nav_active == "menu"){ echo "active__nav" ; }?>">Menu</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>gallery/" class="nav__link <?php if($nav_active == "gallery"){ echo "active__nav" ; }?>">Gallery</a></li>
+                            <li class="nav__list"><a href="<?php echo base_url();?>contact_us/reservation.php" class="nav__link <?php if($nav_active == "reservation"){ echo "active__nav" ; }?>">Reservation</a></li>
+                            <!-- <li class="nav__list"><a href="<?php echo base_url();?>contact_us/" class="nav__link">Contact Us</a></li> -->
                         </ul>
                     </nav>
                 </header>
