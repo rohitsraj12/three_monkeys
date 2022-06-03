d6946640aa148b1166f00db2384e48917c06dfbd
rohitsraj12 <rohitsraj12@gmail.com>
2022-06-03 17:10:52 +0530

    Update index.php

--- website/craft_beers/index.php
+++ website/craft_beers/index.php
@@ -66,7 +66,7 @@
             <main>
 
                 <section class="section__about">
-                    <div class="wrap__container py-5">
+                    <div class="container py-5">
                         <div class="row py-5 align-items-center g-4">
 
                             <div class="col-md-6">
@@ -77,14 +77,14 @@
                                                     data-aos-delay="50"
                                                     data-aos-duration="1000"
                                                     data-aos-easing="ease-in">
-                                            Three Moneys
+                                            Three Monkeys
                                         </p>
                                         <p class="primary__header" data-aos="zoom-in"
                                                     data-aos-offset="100"
                                                     data-aos-delay="50"
                                                     data-aos-duration="1000"
                                                     data-aos-easing="ease-in">
-                                            Craft Beer Name
+                                                    Hefeweizen
                                         </p>
                                     </header>
 
@@ -94,8 +94,17 @@
                                                     data-aos-delay="50"
                                                     data-aos-duration="1000"
                                                     data-aos-easing="ease-in">
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe neque veritatis magni illo inventore sed ducimus, dicta fuga totam et doloribus fugit dignissimos alias deleniti incidunt perspiciatis eius? Laborum, hic. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, voluptates in. Amet dicta placeat quae laboriosam pariatur ratione dignissimos odio maiores voluptatem eius saepe quasi perspiciatis minus mollitia, dolorum assumenda facilis labore qui? Repudiandae dicta voluptatibus exercitationem perspiciatis rem praesentium.
+                                                    Unfiltered wheat beer in the German style. Malty sweet and balanced, with moderate banana clove flavor and strong aroma. Low bitterness and slightly spicy.
                                         </p>
+                                        <ul data-aos="slide-up"
+                                            data-aos-offset="100"
+                                            data-aos-delay="50"
+                                            data-aos-duration="1000"
+                                            data-aos-easing="ease-in">
+                                            <li>Color : Yellow</li>
+                                            <li>Origin : Germany</li>
+                                            <li>Abv : 5.0%</li>
+                                        </ul>
                                        
                                     </div>
 
@@ -104,13 +113,13 @@
 
                             <div class="col-md-1"></div>
 
-                            <div class="col-md-5">
+                            <div class="col-md-5 text-center">
                                 <figure data-aos="fade-in"
                                                     data-aos-offset="100"
                                                     data-aos-delay="50"
                                                     data-aos-duration="1000"
                                                     data-aos-easing="ease-in">
-                                    <img class="w-100" src="<?php echo base_url();?>assets/img/craft-beer/beer-1.jpg" alt="">
+                                    <img class="" src="<?php echo base_url();?>assets/img/craft-beer/beer-5.png" alt="">
                                 </figure>
                             </div>
 
@@ -119,16 +128,16 @@
                 </section>
 
                 <section class="section__about">
-                    <div class="wrap__container py-5">
+                    <div class="container py-5">
                         <div class="row py-5 align-items-center g-4">
 
-                            <div class="col-md-5">
+                            <div class="col-md-5 text-center">
                                 <figure data-aos="fade-in"
                                                     data-aos-offset="100"
                                                     data-aos-delay="50"
                                                     data-aos-duration="1000"
                                                     data-aos-easing="ease-in">
-                                    <img class="w-100" src="<?php echo base_url();?>assets/img/craft-beer/beer-1.jpg" alt="">
+                                    <img class="" src="<?php echo base_url();?>assets/img/craft-beer/beer-4.png" alt="">
                                 </figure>
                             </div>
 
@@ -149,7 +158,7 @@
                                                     data-aos-delay="50"
                                                     data-aos-duration="1000"
                                                     data-aos-easing="ease-in">
-                                            Craft Beer Name
+                                            Weizenbock
                                         </p>
                                     </header>
 
@@ -159,9 +168,17 @@
                                             data-aos-delay="50"
                                             data-aos-duration="1000"
                                             data-aos-easing="ease-in">
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe neque veritatis magni illo inventore sed ducimus, dicta fuga totam et doloribus fugit dignissimos alias deleniti incidunt perspiciatis eius? Laborum, hic. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, voluptates in. Amet dicta placeat quae laboriosam pariatur ratione dignissimos odio maiores voluptatem eius saepe quasi perspiciatis minus mollitia, dolorum assumenda facilis labore qui? Repudiandae dicta voluptatibus exercitationem perspiciatis rem praesentium.
+                                            A strong, malty, fruity, wheat-based ale combining the best malt and yeast flavors of a Hefeweizen with the malty-rich flavor, strength, and body of a Bockbier.
                                         </p>
-                                       
+                                        <ul data-aos="slide-up"
+                                            data-aos-offset="100"
+                                            data-aos-delay="50"
+                                            data-aos-duration="1000"
+                                            data-aos-easing="ease-in">
+                                            <li>Color : Light Orange</li>
+                                            <li>Origin : Germany</li>
+                                            <li>Abv : 8.0%</li>
+                                        </ul>
                                     </div>
 
                                 </article>
@@ -171,6 +188,130 @@
                     </div>
                 </section>
 
+                
+                <section class="section__about">
+                    <div class="container py-5">
+                        <div class="row py-5 align-items-center g-4">
+
+                            <div class="col-md-6">
+                                <article>
+                                    <header class="article__header pb-4">
+                                        <p class="primary__header-sub sub__style" data-aos="zoom-in"
+                                                    data-aos-offset="100"
+                                                    data-aos-delay="50"
+                                                    data-aos-duration="1000"
+                                                    data-aos-easing="ease-in">
+                                            Three Monkeys
+                                        </p>
+                                        <p class="primary__header" data-aos="zoom-in"
+                                                    data-aos-offset="100"
+                                                    data-aos-delay="50"
+                                                    data-aos-duration="1000"
+                                                    data-aos-easing="ease-in">
+                                                    Belgian Session
+                                        </p>
+                                    </header>
+
+                                    <div class="article__body">
+                                        <p data-aos="slide-up"
+                                                    data-aos-offset="100"
+                                                    data-aos-delay="50"
+                                                    data-aos-duration="1000"
+                                                    data-aos-easing="ease-in">
+                                                    A soft, gentle form of pale ale with a gold to copper in color and can have caramel or toasted malt flavor which is typically low and spicing is absent, lacking the Belgian tendency to challenge while retaining the principle of balance.
+                                        </p>
+                                        <ul data-aos="slide-up"
+                                            data-aos-offset="100"
+                                            data-aos-delay="50"
+                                            data-aos-duration="1000"
+                                            data-aos-easing="ease-in">
+                                            <li>Color : Pale Yellow</li>
+                                            <li>Origin : Belgium</li>
+                                            <li>Abv : 5.2%</li>
+                                        </ul>
+                                       
+                                    </div>
+
+                                </article>
+                            </div>
+
+                            <div class="col-md-1"></div>
+
+                            <div class="col-md-5 text-center">
+                                <figure data-aos="fade-in"
+                                                    data-aos-offset="100"
+                                                    data-aos-delay="50"
+                                                    data-aos-duration="1000"
+                                                    data-aos-easing="ease-in">
+                                    <img class="" src="<?php echo base_url();?>assets/img/craft-beer/beer-5.png" alt="">
+                                </figure>
+                            </div>
+
+                        </div>
+                    </div>
+                </section>
+
+
+                <section class="section__about">
+                    <div class="container py-5">
+                        <div class="row py-5 align-items-center g-4">
+
+                            <div class="col-md-5 text-center">
+                                <figure data-aos="fade-in"
+                                                    data-aos-offset="100"
+                                                    data-aos-delay="50"
+                                                    data-aos-duration="1000"
+                                                    data-aos-easing="ease-in">
+                                    <img class="" src="<?php echo base_url();?>assets/img/craft-beer/beer-6.png" alt="">
+                                </figure>
+                            </div>
+
+                            <div class="col-md-1"></div>
+
+                            <div class="col-md-6">
+                                <article>
+                                    <header class="article__header pb-4">
+                                        <p class="primary__header-sub sub__style" data-aos="zoom-in"
+                                                    data-aos-offset="100"
+                                                    data-aos-delay="50"
+                                                    data-aos-duration="1000"
+                                                    data-aos-easing="ease-in">
+                                            Three Moneys
+                                        </p>
+                                        <p class="primary__header" data-aos="zoom-in"
+                                                    data-aos-offset="100"
+                                                    data-aos-delay="50"
+                                                    data-aos-duration="1000"
+                                                    data-aos-easing="ease-in">
+                                                    Red Ale
+                                        </p>
+                                    </header>
+
+                                    <div class="article__body">
+                                        <p data-aos="slide-up"
+                                            data-aos-offset="100"
+                                            data-aos-delay="50"
+                                            data-aos-duration="1000"
+                                            data-aos-easing="ease-in">
+                                            The Irish-style red ale is a balanced beer that uses a moderate amount of kilned malts and roasted barley in the recipe, which gives the beer the color for which it is named.
+                                        </p>
+                                        <ul data-aos="slide-up"
+                                            data-aos-offset="100"
+                                            data-aos-delay="50"
+                                            data-aos-duration="1000"
+                                            data-aos-easing="ease-in">
+                                            <li>Color : Deep Red</li>
+                                            <li>Origin : America</li>
+                                            <li>Abv : 6.0%</li>
+                                        </ul>
+                                    </div>
+
+                                </article>
+                            </div>
+
+                        </div>
+                    </div>
+                </section>
             </main>
         </div>
         <!-- end body container -->
