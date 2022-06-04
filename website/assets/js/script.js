b4b407118c939f1ebb4208eee230b0aa9a45633c
rohitsraj12 <rohitsraj12@gmail.com>
2022-06-04 17:24:43 +0530

    Update script.js

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(".hamberger").click(function(){
         // toggle type one
         $(".header__nav").toggle(function(){
-            $(".header__nav").css("right", "100%"); 
-            $("body").css("overflow", "hidden");     
+            $(".header__nav").css("right", "100%");       
         }, function(){
-            $(".header__nav").css("left", "0");     
-        })
+            $(".header__nav").css("left", "0");      
+        });
+
 
         // toggle type 2
         // $(".header__nav").toggle().css("right", "0");
