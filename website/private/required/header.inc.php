b704ffea7cf666e7fe0a81f09cf4e28acbbf32e0
rohitsraj12 <rohitsraj12@gmail.com>
2022-06-22 17:43:22 +0530

    Update header.inc.php

--- website/private/required/header.inc.php
+++ website/private/required/header.inc.php
@@ -6,6 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
 
+    <meta name="description" content="<?php if(empty($meta_desc)){echo "Three Monkeys";} else { echo $meta_desc;} ?>">
+    <meta name="keywords" content="<?php if(empty($meta_key)){echo "Three Monkeys";} else {echo $meta_key;}; ?>">
+    <meta name="author" content="Rohit">
+    <meta name="distribution" content="global" />
+    <meta http-equiv="content-language" content="en-gb">
+    <meta name="city" content="bangalore" />
+    <meta name="geo.region" content="IN" />
+    <meta name="state" content="Karnataka" />
+    <!-- <meta name="twitter:card" content="summary">
+    <meta name="twitter:creator" content="@"/>
+    <meta name="twitter:description" content="<?php echo $meta_key; ?>" />
+    <meta name="twitter:keywords" content="<?php echo $meta_desc; ?>"> -->
+
+    <!-- open graph code begins-->
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="<?php echo $page_title; ?>">
+    <meta property="og:url" content="<?php echo base_url();?>">
+    <meta property="business:contact_data:street_address" content="Old Madrass Road">
+    <meta property="business:contact_data:locality" content="Bangalore">
+    <meta property="business:contact_data:region" content="Karnataka">
+    <meta property="business:contact_data:postal_code" content="">
+    <meta property="business:contact_data:country_name" content="India">
+
+    <!-- hide url from search engine -->
+    <!-- <meta name="robots" content="noindex"> -->
+
+    <!-- sitemap generation code -->
+    <meta name="google-site-verification" content="9pZtm1Uyp6IKNmNmHb0G-GGgNlc5CqVuPmTeb-IQe1c" />
+    
+    <link rel="icon" href="<?php echo base_url();?>assets/img/brand/fevicon.png" type="image/x-icon">
+
     <!-- stylesheet -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
@@ -17,7 +48,13 @@
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/center_nav.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/gallery.css">
+    <?php 
+        if($nav_active == "gallery"){
+            ?>
+                <link rel="stylesheet" href="<?php echo base_url();?>assets/css/gallery.css">
+            <?php
+        }
+    ?>
     
     <!-- script -->
 </head>
@@ -38,6 +75,7 @@
             <div class="body__header-top">
                 <div class="header__container-center">
                     <div class="row">
+
                         <div class="col-8">
                             <ul class="header__contact" data-aos="fade-in"
                                     data-aos-offset="100"
@@ -51,6 +89,7 @@
                             </ul>
                         </div>
                         <!-- end header contact -->
+
                         <div class="col-4">
                             <ul class="header__social" data-aos="fade-in"
                                     data-aos-offset="100"
@@ -63,6 +102,7 @@
                                 <li class="header__social-list"><a href="https://www.youtube.com/channel/UCo_qzSCAN1sxOYixOfEt_qw" target="_blank" class="header__social-link"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
                             </ul>
                             <!-- end header social -->
+                            
                         </div>
                     </div>
                 </div>
