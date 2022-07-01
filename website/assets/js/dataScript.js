39291b7ae135c8145c0ba9b0b0638d35f97d9f56
rohitsraj12 <rohitsraj12@gmail.com>
2022-07-01 17:26:07 +0530

    Update dataScript.js

--- website/assets/js/dataScript.js
+++ website/assets/js/dataScript.js
@@ -1,24 +1,28 @@
 $(document).ready(function(){
+
+
+
+
     $(".header__contact .address").text('Gopalan Mall, Old Madras Road');
     $(".header__contact .tel").text('994 504 0519');
     $(".header__contact .mailto").text('gm.3monkeys@gmail.com');
-    $(".header__contact .timing").text('10:00am to 11.30pm');
+    $(".header__contact .timing").text('02:00pm to 11.00pm');
 
     //footer
     $(".footer__contact .address").html('4th Floor, Gopalan Signature Mall,</br> Old Madras Road, Bengaluru 560038');
     $(".footer__contact .tel").text('994 504 0519');
     $(".footer__contact .mailto").text('gm.3monkeys@gmail.com');
-    $(".footer__contact .timing").text('10:00am to 11.30pm');
+    $(".footer__contact .timing").text('02:00pm to 11.00pm');
 
     //contact us page
     $(".section__contact_us .address").text('4th Floor, Gopalan Signature Mall, Old Madras Road, Bengaluru 560038');
     $(".section__contact_us .tel").text('994 504 0519');
     $(".section__contact_us .mailto").text('gm.3monkeys@gmail.com');
-    $(".section__contact_us .timing").text('10:00am to 11.30pm');
+    $(".section__contact_us .timing").text('02:00pm to 11.00pm');
     
     //reservation page
     $(".reservation__contact .address").text('4th Floor, Gopalan Signature Mall, Old Madras Road, Bengaluru 560038');
     $(".reservation__contact .tel").text('994 504 0519');
     $(".reservation__contact .mailto").text('gm.3monkeys@gmail.com');
-    $(".reservation__contact .timing").text('10:00am to 11.30pm');
+    $(".reservation__contact .timing").text('02:00pm to 11.00pm');
 })
