a5dd209ce4012a6593d64a2709c18c7b73d67875
rohitsraj12 <rohitsraj12@gmail.com>
2022-07-18 16:54:24 +0530

    Update header.inc.php

--- website/private/required/header.inc.php
+++ website/private/required/header.inc.php
@@ -54,6 +54,12 @@
                 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/gallery.css">
             <?php
         }
+
+        if($nav_active == "home"){
+            ?>
+                <meta name="facebook-domain-verification" content="ygxk26mxkqfpo9hxtu7cbsogt0u05h" />  
+            <?php
+        }
     ?>
     
     <!-- script -->
@@ -67,6 +73,29 @@
             })(window,document,'script','dataLayer','GTM-KX8HWSQ');
         </script>
     <!-- End Google Tag Manager -->
+
+
+    <!-- facebook script -->
+        <!-- Meta Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '517463370156761');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=517463370156761&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Meta Pixel Code -->
+    <!-- end facebook script -->
+
+
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
